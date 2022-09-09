75701785ab26bbafb526705df1c450062e3dcedb
Moriyarnn <sebastian_verdugo@hotmail.com>
2022-09-09 00:24:50 -0300

    Added randomgen branch features

--- Assets/Scripts/UI/SpawnMenu.cs
+++ Assets/Scripts/UI/SpawnMenu.cs
@@ -20,14 +20,13 @@ public class SpawnMenu : MonoBehaviour
 
     // Spawn Panel Interactable UI Components
     [SerializeField] private TextScrollView optionTexturePath;
-    [SerializeField] private TextToggle optionGravity; 
+    [SerializeField] private TextToggle optionGravity;
     [SerializeField] private TextDropdown optionObjectType;
     [SerializeField] private TextSlider optionObjectSize;
     [SerializeField] private Button spawnButton;
 
     // Auxiliary Task Class
     [SerializeField] private GameObject fileLoadManager;
-    private List<Texture> retrievedTextures;
 
     // Coroutine status
     private bool spawnObjectRunning;
@@ -50,20 +49,11 @@ public class SpawnMenu : MonoBehaviour
         // Spawn operation has to end before initiating another one (CHANGE: Show error message in the panel)
         if (!spawnObjectRunning)
         {
-            StartCoroutine(AutomaticSpawner());
+            StartCoroutine("SpawnObject");
         }
         else
         {
             Debug.Log("There is an object spawning operation at the moment, please wait");
-        }
-    }
-
-    public IEnumerator AutomaticSpawner()
-    {
-        while(true)
-        {
-            yield return StartCoroutine(SpawnObject());
-            yield return new WaitForSeconds(2);
         }
     }
 
@@ -76,66 +66,21 @@ public class SpawnMenu : MonoBehaviour
         List<string> texturePaths = optionTexturePath.scrollViewPaths;
 
         // Try retrieving texture with scrollview paths
-        if(retrievedTextures == null)
+        List<Texture> retrievedTextures = null;
+        LoadLocalManager loadManager = Instantiate(fileLoadManager).GetComponent<LoadLocalManager>();
+        yield return StartCoroutine(loadManager.RetrieveTexture(texturePaths));
+        if (loadManager.result == Result.Success)
         {
-            LoadLocalManager loadManager = Instantiate(fileLoadManager).GetComponent<LoadLocalManager>();
-            yield return StartCoroutine(loadManager.RetrieveTexture(texturePaths));
-            if (loadManager.result == Result.Success)
-            {
-                Destroy(loadManager.gameObject);
+            Destroy(loadManager.gameObject);
 
-                retrievedTextures = loadManager.retrievedTextures;
-                // Generate list of positions CHANGE: For now, just add + 1 to x value, change this when implementing spatial placement
-                yield return StartCoroutine(GenerateObjectPlacement(10));
-                // For each file create an object
-                for (int i = 0; i < 10; i++)
-                {
-                    // Spawning starts from instantiating the prefab
-                    GameObject spawnPrefab = spawnPrefabs[Random.Range(0, 5)];
-                    GameObject spawnObject = Instantiate(spawnPrefab, spawnPositions[i], spawnPrefab.transform.rotation, spawnGroup);
-
-                    // Get the data from menu components
-                    bool hasGravity = optionGravity.GetData();
-                    float sizeMultiplier = optionObjectSize.GetData();
-
-                    // Apply data
-                    // Apply gravity toggle
-                    if (!hasGravity)
-                    {
-                        spawnObject.GetComponent<Rigidbody>().isKinematic = true;
-                    }
-                    // Apply size slider
-                    spawnObject.transform.localScale *= sizeMultiplier;
-                    // Apply material file
-                    spawnObject.GetComponent<Renderer>().material.mainTexture = retrievedTextures[Random.Range(0, retrievedTextures.Count)];
-                }
-            }
-            else
-            {
-                Destroy(loadManager.gameObject);
-
-                if (loadManager.result == Result.WebRequestError)
-                {
-                    //CHANGE:TO SHOW IN UI
-                    Debug.Log("Could not retrieve texture (Webrequest error)");
-                }
-                else if (loadManager.result == Result.TypeError)
-                {
-                    //CHANGE:TO SHOW IN UI
-                    // Supported extensions
-                    Debug.Log("One or more files have unsupported extension, please try: .jpg, .png");
-                }
-            }
-        }
-        else
-        {
+            retrievedTextures = loadManager.retrievedTextures;
             // Generate list of positions CHANGE: For now, just add + 1 to x value, change this when implementing spatial placement
-            yield return StartCoroutine(GenerateObjectPlacement(10));
+            yield return StartCoroutine(GenerateObjectPlacement(retrievedTextures.Count));
             // For each file create an object
-            for (int i = 0; i < 10; i++)
+            for (int i = 0; i < retrievedTextures.Count; i++)
             {
                 // Spawning starts from instantiating the prefab
-                GameObject spawnPrefab = spawnPrefabs[Random.Range(0, 5)];
+                GameObject spawnPrefab = spawnPrefabs[optionObjectType.GetData()];
                 GameObject spawnObject = Instantiate(spawnPrefab, spawnPositions[i], spawnPrefab.transform.rotation, spawnGroup);
 
                 // Get the data from menu components
@@ -151,10 +96,25 @@ public class SpawnMenu : MonoBehaviour
                 // Apply size slider
                 spawnObject.transform.localScale *= sizeMultiplier;
                 // Apply material file
-                spawnObject.GetComponent<Renderer>().material.mainTexture = retrievedTextures[Random.Range(0, retrievedTextures.Count)];
+                spawnObject.GetComponent<Renderer>().material.mainTexture = retrievedTextures[i];
             }
         }
-       
+        else
+        {
+            Destroy(loadManager.gameObject);
+
+            if (loadManager.result == Result.WebRequestError)
+            {
+                //CHANGE:TO SHOW IN UI
+                Debug.Log("Could not retrieve texture (Webrequest error)");
+            }
+            else if (loadManager.result == Result.TypeError)
+            {
+                //CHANGE:TO SHOW IN UI
+                // Supported extensions
+                Debug.Log("One or more files have unsupported extension, please try: .jpg, .png");
+            }
+        }
 
         loadingSmall.DoneLoading();
         spawnObjectRunning = false;
@@ -163,15 +123,12 @@ public class SpawnMenu : MonoBehaviour
     public IEnumerator GenerateObjectPlacement(int numberOfPlacements) //CHANGE: Take an option and split this function for each placement mode
     {
         spawnPositions = new List<Vector3>();
-        float range = 15.0f;
         for (int i = 0; i < numberOfPlacements; i++)
         {
-            Vector3 randomVector = new Vector3(Random.Range(-range, range), 20, Random.Range(-range, range));
-
-            Vector3 placementPosition = Vector3.zero + randomVector;
+            Vector3 placementPosition = spawnCenter.position + Vector3.right * i;
             spawnPositions.Add(placementPosition);
             yield return null;
         }
-    } 
+    }
 
 }
